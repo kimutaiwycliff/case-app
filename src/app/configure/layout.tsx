2a358fb197e8ab921c16384c3f92432df63bc16f
Wycliff Kimutai <kcliff@tutanota.com>
2024-10-14 15:48:49 +0300

    steps component

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,13 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper"
-import { ReactNode } from "react"
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Steps from '@/components/Steps';
+import { ReactNode } from 'react';
 
-const layout = ({children}:{children:ReactNode}) => {
+const layout = ({ children }: { children: ReactNode }) => {
   return (
-   <MaxWidthWrapper className="flex-1 flex flex-col ">{children}</MaxWidthWrapper>
-  )
-}
-export default layout
+    <MaxWidthWrapper className="flex-1 flex flex-col ">
+      <Steps />
+      {children}
+    </MaxWidthWrapper>
+  );
+};
+export default layout;
