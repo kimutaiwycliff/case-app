76ea422428ebc441b75cd298b3ca8f6cbdc7664e
Wycliff Kimutai <kcliff@tutanota.com>
2024-10-14 09:37:58 +0300

    section user review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from '@/components/Icons';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Phone from '@/components/Phone';
 import { Check, Star } from 'lucide-react';
@@ -91,16 +92,117 @@ const Home = () => {
                 alt="Phone case"
                 width={400}
                 height={400}
-                className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
               />
               <Image
                 src="/line.png"
                 alt="Phone case"
                 width={400}
                 height={400}
-                className='absolute w-20 -left-6 -bottom-6 select-none'
+                className="absolute w-20 -left-6 -bottom-6 select-none"
               />
-              <Phone imgSrc="/testimonials/1.jpg" className='w-64' />
+              <Phone imgSrc="/testimonials/1.jpg" className="w-64" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{' '}
+              <span className="relative px-2 ">
+                customers{' '}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{' '}
+              say
+            </h2>
+            <Image
+              src="/snake-2.png"
+              alt=""
+              width={400}
+              height={400}
+              className="w-24 order-0 lg:order-2"
+            />
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16 ">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20 ">
+              <div className="flex gap-0.5 mb-2 ">
+                {stars.map((_, index) => (
+                  <Star
+                    key={index}
+                    className="w-5 h-5 text-green-600 fill-green-600"
+                  />
+                ))}
+              </div>
+              <div className="text-lg leading-8 ">
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half months now and{' '}
+                  <span className="p-0.5 bg-slate-800 text-white rounded-xl">
+                    {' '}
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple of weeks. Love it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2 ">
+                <Image
+                  src="/users/user-1.png"
+                  alt=""
+                  className="rounded-full h-12 w-12 object-cover"
+                  width={400}
+                  height={400}
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="w-4 h-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Second User Review */}
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16 ">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20 ">
+              <div className="flex gap-0.5 mb-2 ">
+                {stars.map((_, index) => (
+                  <Star
+                    key={index}
+                    className="w-5 h-5 text-green-600 fill-green-600"
+                  />
+                ))}
+              </div>
+              <div className="text-lg leading-8 ">
+                <p>
+                  "I usually keep my phone together with my keys and that led to
+                  some pretty heavy scratch marks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner,{' '}
+                  <span className="p-0.5 bg-slate-800 text-white rounded-xl">
+                    {' '}
+                    looks brand new after about half an year
+                  </span>
+                  .I dig it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2 ">
+                <Image
+                  src="/users/user-4.jpg"
+                  alt=""
+                  className="rounded-full h-12 w-12 object-cover"
+                  width={400}
+                  height={400}
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Josh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="w-4 h-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
